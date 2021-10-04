8549a1f7ae798fef06232a82c942fc193b037a88
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-10-05 00:48:03 +0800

    added a doc

--- functions/api/setupDb.js
+++ functions/api/setupDb.js
@@ -12,6 +12,7 @@ async function setupDatabase(req, res, next) {
     { paymentName: "Aeon",paymentDate:admin.firestore.Timestamp.fromDate(new Date('05/20/2021')),logo:"https://th.bing.com/th/id/R.34c53219f6c6816884112b4f8e14934d?rik=dLco8TIoTC64sA&pid=ImgRaw&r=0",amount:44.0,type:"payment"},
     { paymentName: "Tesco",paymentDate:admin.firestore.Timestamp.fromDate(new Date('02/30/2021')),logo:"https://seekcdn.com/pacman/company-profiles/logos/432826/7-eleven-logo.png",amount:73.0,type:"payment"},
     { paymentName: "7 Eleven",paymentDate:admin.firestore.Timestamp.fromDate(new Date('02/10/2021')),logo:"https://th.bing.com/th/id/OIP.MsEV99SIf-qV0mUDfn1akgHaFg?pid=ImgDet&rs=1",amount:20.0,type:"reload"},
+    { paymentName: "Giant",paymentDate:admin.firestore.Timestamp.fromDate(new Date('03/10/2021')),logo:"https://th.bing.com/th/id/OIP.adSdSUd_iPt928oOUW3LUgHaD1?pid=ImgDet&rs=1",amount:44.0,type:"payment"},
    
     
     
